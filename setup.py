098df7f77a3eb37cee8696df9a6359a41e23964e
Liam Henrickson <liam.henrickson@gmail.com>
2020-07-09 14:26:58 -0600

    Bug fix & bump version

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 from pathlib import Path
 
 
 setup(name='convenience-collection',
-      version='1.0.0',
+      version='1.0.1',
       author="Liam Henrickson",
       author_email="liam.henrickson@gmail.com",
       license="MIT License",
       url="https://github.com/HLiam/ConvenienceCollection",
       long_description=Path('README.md').read_text(),
       long_description_content_type='text/markdown',
-      py_modules=['convenience_collection'],
+      packages=find_packages(),
       install_requires=['colorama',
                         'win10toast ; platform_system=="Windows"'],
       classifiers=['Programming Language :: Python :: 3',
